ee5e14ec0be7b3c8a499c2bf332568a22a169701
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2023-05-03 23:27:51 +0100

    Add tests

---
+++ tests/unit_tests/test_check_email.py
@@ -0,0 +1,13 @@
+import server
+
+client = server.app.test_client()
+
+
+def test_known_email():
+    result = client.post("/show_summary", data=dict(email="john@simplylift.co"))
+    assert result.status_code == 200
+
+
+def test_unknown_email():
+    result = client.post("/show_summary", data=dict(email="account@domain.com"))
+    assert result.status_code == 302
